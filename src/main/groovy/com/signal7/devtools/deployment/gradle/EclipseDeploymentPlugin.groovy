6deaa88f8d8d0ba96319cef7c67356d8cd6b5b75
Max Bruchmann <bruchmann@signal7.de>
2013-09-03 19:00:12 +0200

    changed copy to project.copy

--- src/main/groovy/com/signal7/devtools/deployment/gradle/EclipseDeploymentPlugin.groovy
+++ src/main/groovy/com/signal7/devtools/deployment/gradle/EclipseDeploymentPlugin.groovy
@@ -3,6 +3,7 @@ package com.signal7.devtools.deployment.gradle;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.Copy;
+import org.gradle.api.tasks.Delete;
 
 class EclipseDeploymentPlugin implements Plugin<Project>{
 
@@ -29,17 +30,22 @@ class EclipseDeploymentPlugin implements Plugin<Project>{
 
 
 
-		project.task("provideWarLibs", type:Copy){
+		def provideWarLibsTask = project.task("provideWarLibs")<<{
 
 			def warLibs = project.configurations.runtime - project.configurations.providedRuntime
 
 			project.deploymentDescriptor.providedProjects.each { p ->
 				warLibs -= p.tasks["jar"].outputs.files
 			}
-			from warLibs
-			into project.deploymentDescriptor.warLibsDir
+			project.copy {
+				from warLibs
+				into project.deploymentDescriptor.warLibsDir
+			}
 		}
+		provideWarLibsTask.outputs.dir project.deploymentDescriptor.warLibsDir
 		project.tasks.provideWarLibs.dependsOn(project.tasks.war)
+
+		project.task("cleanWarLibs", type: Delete){ delete provideWarLibsTask }
 
 		// fixme
 		outputDeploymentFileTask.outputs.upToDateWhen {false}
