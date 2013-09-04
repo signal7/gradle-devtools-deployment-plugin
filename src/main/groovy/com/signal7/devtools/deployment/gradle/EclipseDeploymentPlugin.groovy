ba8d3fb95089e1def63501995bf1eea4bf1dcb80
Max Bruchmann <bruchmann@signal7.de>
2013-09-04 11:44:04 +0200

    provideWarLibs only if war plugin is available

--- src/main/groovy/com/signal7/devtools/deployment/gradle/EclipseDeploymentPlugin.groovy
+++ src/main/groovy/com/signal7/devtools/deployment/gradle/EclipseDeploymentPlugin.groovy
@@ -2,6 +2,7 @@ package com.signal7.devtools.deployment.gradle;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.plugins.WarPlugin;
 import org.gradle.api.tasks.Copy;
 import org.gradle.api.tasks.Delete;
 
@@ -26,28 +27,32 @@ class EclipseDeploymentPlugin implements Plugin<Project>{
 			outputFile.text=writer.toString()
 		}
 		project.tasks.eclipse.dependsOn(outputDeploymentFileTask)
+		
 		outputDeploymentFileTask.outputs.file outputFile
-
-
-
-		def provideWarLibsTask = project.task("provideWarLibs")<<{
-
-			def warLibs = project.configurations.runtime - project.configurations.providedRuntime
-
-			project.deploymentDescriptor.providedProjects.each { p ->
-				warLibs -= p.tasks["jar"].outputs.files
-			}
-			project.copy {
-				from warLibs
-				into project.deploymentDescriptor.warLibsDir
-			}
-		}
-		provideWarLibsTask.outputs.dir project.deploymentDescriptor.warLibsDir
-		project.tasks.provideWarLibs.dependsOn(project.tasks.war)
-
-		project.task("cleanWarLibs", type: Delete){ delete provideWarLibsTask }
-
+		
+		
+		
 		// fixme
 		outputDeploymentFileTask.outputs.upToDateWhen {false}
+
+		if (project.plugins.hasPlugin(WarPlugin)) {
+			def provideWarLibsTask = project.task("provideWarLibs")<<{
+
+				def warLibs = project.configurations.runtime - project.configurations.providedRuntime
+
+				project.deploymentDescriptor.providedProjects.each { p ->
+					warLibs -= p.tasks["jar"].outputs.files
+				}
+				project.copy {
+					from warLibs
+					into project.deploymentDescriptor.warLibsDir
+				}
+			}
+			provideWarLibsTask.outputs.dir project.deploymentDescriptor.warLibsDir
+			project.tasks.provideWarLibs.dependsOn(project.tasks.war)
+
+			project.task("cleanWarLibs", type: Delete){ delete provideWarLibsTask }
+		}
+
 	}
 }
